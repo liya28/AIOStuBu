bf9fd5f88d804d34ff972ee26575d477f0d16a10
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-10-28 16:38:37 +0800

    commit

--- StewBoo/src/main/java/io/serateam/stewboo/ui/StewBooApplication.java
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/StewBooApplication.java
@@ -1,27 +1,52 @@
 package io.serateam.stewboo.ui;
 
-import atlantafx.base.theme.PrimerLight;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class StewBooApplication extends Application {
+    private double window_x = 0;
+    private double window_y = 0;
 
     public static void StewBooLaunch() {
         launch();
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
-        FXMLLoader fxmlLoader = new FXMLLoader(StewBooApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws NullPointerException {
+        try {
+            FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(FxmlVariables.url_path_dashboardFxml));
+            Parent root = loader.load();
+
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+
+            // Enable window dragging
+            root.setOnMousePressed(event -> {
+                window_x = event.getSceneX();
+                window_y = event.getSceneY();
+            });
+
+            root.setOnMouseDragged(event -> {
+                primaryStage.setX(event.getScreenX() - window_x);
+                primaryStage.setY(event.getScreenY() - window_y);
+            });
+
+            Scene scene = new Scene(root, 800, 500);
+
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("css/style.css")).toExternalForm());
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
