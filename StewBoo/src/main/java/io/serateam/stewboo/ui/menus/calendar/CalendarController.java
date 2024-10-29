ab0bbbbc3fa5a02c454c9272746b31b91cb9a23a
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-10-30 02:55:19 +0800

    added skeleton classes

---
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/menus/calendar/CalendarController.java
@@ -0,0 +1,13 @@
+package io.serateam.stewboo.ui.menus.calendar;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CalendarController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        throw new RuntimeException("Uninitialized method exception");
+    }
+}
---
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/menus/calendar/CalendarMenu.java
@@ -0,0 +1,6 @@
+package io.serateam.stewboo.ui.menus.calendar;
+
+import io.serateam.stewboo.ui.menus.IMenu;
+
+class CalendarMenu implements IMenu {
+}
