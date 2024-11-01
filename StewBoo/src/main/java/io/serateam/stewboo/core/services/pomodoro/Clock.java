130128e0fe0173026bedf2094f78171cc64b7a57
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-01 11:56:52 +0800

    create clock class

---
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/Clock.java
@@ -0,0 +1,16 @@
+package io.serateam.stewboo.core.services.pomodoro;
+
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class Clock
+{
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    Clock()
+    {
+
+    }
+
+//    private void
+}
