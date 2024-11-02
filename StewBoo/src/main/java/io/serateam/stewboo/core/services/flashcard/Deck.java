a741d71c91700623b13c3c62f3eac2ad12944773
muhadma <harleyreyes456@gmail.com>
2024-11-03 00:40:39 +0800

    added Deck fields and methods

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/flashcard/Deck.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/flashcard/Deck.java
@@ -1,7 +1,40 @@
 package io.serateam.stewboo.core.services.flashcard;
 
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
-class Deck {
-    Stack<Card> deck;
+public class Deck {
+    private String name;
+    private List<Card> flashCards;
+
+    public Deck(String name)
+    {
+        this.name = name;
+        this.flashCards = new ArrayList<>();
+    }
+
+    public void addflashCard(Card flashCard)
+    {
+        flashCards.add(flashCard);
+    }
+
+    public int getflashCardsCount()
+    {
+        return flashCards.size();
+    }
+
+    public List<Card> getflashCards()
+    {
+        return flashCards;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void clearflashcards()
+    {
+        flashCards.clear();
+    }
 }
