4f40dcdf1c3b009f0269ca08cf372ed2a6b4d718
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-02 10:55:30 +0800

    added initializer for core main

--- StewBoo/src/main/java/io/serateam/stewboo/core/StewBoo.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/StewBoo.java
@@ -1,6 +1,17 @@
 package io.serateam.stewboo.core;
 
-// set up processes go here (config setup, authentication, file integrity checks, etc)
-public class StewBoo {
+import io.serateam.stewboo.core.utility.JSONService;
+import io.serateam.stewboo.core.utility.Sample;
+import io.serateam.stewboo.core.utility.SharedVariables;
 
+// set up processes go here (config setup, authentication, file integrity checks, etc)
+public class StewBoo
+{
+    public static void initializeCore()
+    {
+        Sample s = new Sample();
+        JSONService.serializeAndWriteToFile(SharedVariables.test, s);
+        Sample newS = JSONService.deserialize(SharedVariables.test, Sample.class);
+        System.out.println(newS);
+    }
 }
