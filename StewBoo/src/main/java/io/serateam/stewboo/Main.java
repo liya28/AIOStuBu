80a5bc1e8b99c6ff8160584e21ccd2323cdfc183
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-02 10:56:54 +0800

    added connection to main method to core package main

--- StewBoo/src/main/java/io/serateam/stewboo/Main.java
+++ StewBoo/src/main/java/io/serateam/stewboo/Main.java
@@ -1,9 +1,11 @@
 package io.serateam.stewboo;
 
+import io.serateam.stewboo.core.StewBoo;
 import io.serateam.stewboo.ui.StewBooApplication;
 
 public class Main {
     public static void main(String[] args) {
+        StewBoo.initializeCore();
         StewBooApplication.StewBooLaunch();
     }
 }
