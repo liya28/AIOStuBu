a731fc6ac21757e480e4296c899062c53134644e
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-16 17:41:27 +0800

    added onTimerUpdate()

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/IPomodoroListener.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/IPomodoroListener.java
@@ -2,4 +2,9 @@ package io.serateam.stewboo.core.services.pomodoro;
 
 public interface IPomodoroListener
 {
+    /**
+     * Updates the listener to the new state of the Pomodoro clock timer.
+     * @param newRemainingSeconds contains the state of the Pomodoro clock timer (in seconds).
+     */
+    void onTimerUpdate(long newRemainingSeconds);
 }
