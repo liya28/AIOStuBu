eadd369dbb64256484917c4bd176063ace235bd7
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-16 17:43:16 +0800

    added JavaDocs for the class

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/PomodoroService.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/PomodoroService.java
@@ -4,6 +4,9 @@ import io.serateam.stewboo.core.services.IService;
 
 // NOTE: PomodoroService is a utility class, hence, it shall be static
 
+/**
+ * Utility class. Controls the entire Pomodoro session of the application.
+ */
 public class PomodoroService implements IService
 {
 
