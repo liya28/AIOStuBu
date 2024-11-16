0f9e448d75a3039df6cfee3efaf35ba76e0dd49a
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-16 17:43:01 +0800

    added note

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/PomodoroService.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/PomodoroService.java
@@ -2,7 +2,7 @@ package io.serateam.stewboo.core.services.pomodoro;
 
 import io.serateam.stewboo.core.services.IService;
 
-import java.time.format.DateTimeFormatter;
+// NOTE: PomodoroService is a utility class, hence, it shall be static
 
 public class PomodoroService implements IService
 {
