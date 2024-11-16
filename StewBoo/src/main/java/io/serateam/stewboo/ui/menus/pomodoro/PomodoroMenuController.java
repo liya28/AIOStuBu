85e9233e9beee4b51ae9bc392f4af9e68bbe910a
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-16 17:50:17 +0800

    added PomodoroMenuController

---
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/menus/pomodoro/PomodoroMenuController.java
@@ -0,0 +1,19 @@
+package io.serateam.stewboo.ui.menus.pomodoro;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import io.serateam.stewboo.core.services.pomodoro.PomodoroService;
+import io.serateam.stewboo.core.services.pomodoro.IPomodoroListener;
+import io.serateam.stewboo.core.services.pomodoro.PomodoroSessionState;
+import io.serateam.stewboo.ui.menus.IMenu;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class PomodoroMenuController implements Initializable, IMenu, IPomodoroListener
+{
+}
