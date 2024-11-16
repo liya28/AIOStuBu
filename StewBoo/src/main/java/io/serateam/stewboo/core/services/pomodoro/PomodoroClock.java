35803015bc30113e6fddb8cfa4f0d649d4e628c6
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-16 17:46:37 +0800

    added private fields

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/PomodoroClock.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/pomodoro/PomodoroClock.java
@@ -12,4 +12,13 @@ class PomodoroClock
     private static final int DEFAULT_POMODORO_MINUTES = 25*60;
     private static final int DEFAULT_POMODORO_BREAK_TIME = 5*60;
     private static final int DEFAULT_POMODORO_LONG_BREAK_TIME = 10*60;
+    private static PomodoroClock instance;
+
+    private final List<IPomodoroListener> pomodoroListeners = new ArrayList<>();
+    private Timer timer;
+    private int pomodoroCounter;
+    private long remainingSeconds;
+    private boolean isBreak;
+    private boolean isRunning;
+    private PomodoroSessionState currentState;
 }
