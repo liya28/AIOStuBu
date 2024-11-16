7c512c8b5b6beaa36afe10d747cbe5c94682b789
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-16 17:35:57 +0800

    support for reflections for the Pomodoro feature

--- StewBoo/src/main/java/module-info.java
+++ StewBoo/src/main/java/module-info.java
@@ -6,6 +6,10 @@ module io.serateam.stewboo {
     requires com.google.gson;
 
     opens io.serateam.stewboo.ui to javafx.fxml;
+    opens io.serateam.stewboo.ui.menus to javafx.fxml;
+    opens io.serateam.stewboo.ui.menus.pomodoro to javafx.fxml;
     opens io.serateam.stewboo.core.utility to com.google.gson;
     exports io.serateam.stewboo.ui;
+    exports io.serateam.stewboo.ui.menus;
+    exports io.serateam.stewboo.ui.menus.pomodoro;
 }
