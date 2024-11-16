8aabe1231c37aeb876a4fb7de30d77ef0ae2b96e
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-16 22:48:26 +0800

    new variables for SharedVariables

--- StewBoo/src/main/java/io/serateam/stewboo/ui/SharedVariables.java
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/SharedVariables.java
@@ -3,12 +3,17 @@ package io.serateam.stewboo.ui;
 import java.net.URL;
 
 public class SharedVariables {
-    public static final String path_dashboardFxml = "controls/dashboard.fxml";
-    public static final String path_calendarFxml = "controls/calendar_view.fxml";
-    public static final String path_flashcardsFxml = "controls/flashcards_view.fxml";
-    public static final String path_pomodoroFxml = "controls/pomodoro_view.fxml";
-    public static final String path_todolistFxml = "controls/todolist_view.fxml";
-    public static final String path_notesFxml = "controls/notes_view.fxml";
+    public static final String path_directoryAudio = "audio/";
+    public static final String path_directoryControls = "controls/";
+    public static final String path_directoryCSS = "css/";
+    public static final String path_directoryImages = "images/";
+
+    public static final String path_dashboardFxml = path_directoryControls + "dashboard.fxml";
+    public static final String path_calendarFxml = path_directoryControls + "calendar_view.fxml";
+    public static final String path_flashcardsFxml = path_directoryControls + "flashcards_view.fxml";
+    public static final String path_pomodoroFxml = path_directoryControls + "pomodoro_view.fxml";
+    public static final String path_todolistFxml = path_directoryControls + "todolist_view.fxml";
+    public static final String path_notesFxml = path_directoryControls + "notes_view.fxml";
 
     public static final URL url_path_dashboardFxml = SharedVariables.class.getResource(path_dashboardFxml);
     public static final URL url_path_calendarFxml = SharedVariables.class.getResource(path_calendarFxml);
@@ -16,4 +21,6 @@ public class SharedVariables {
     public static final URL url_path_pomodoroFxml = SharedVariables.class.getResource(path_pomodoroFxml);
     public static final URL url_path_todolistFxml = SharedVariables.class.getResource(path_todolistFxml);
     public static final URL url_path_notesFxml = SharedVariables.class.getResource(path_notesFxml);
+
+    public static final URL url_path_alarmMp3 = SharedVariables.class.getResource(path_directoryAudio + "pomodoro_alarm.mp3");
 }
