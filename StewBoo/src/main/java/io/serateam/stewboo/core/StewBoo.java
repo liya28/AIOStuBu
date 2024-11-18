a2104c82b3350846650fab33b04e5d2497b8c69d
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-18 17:56:17 +0800

    added initialize() calls from IService classes

--- StewBoo/src/main/java/io/serateam/stewboo/core/StewBoo.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/StewBoo.java
@@ -1,17 +1,33 @@
 package io.serateam.stewboo.core;
 
+import io.serateam.stewboo.core.services.IService;
+import io.serateam.stewboo.core.services.pomodoro.PomodoroService;
 import io.serateam.stewboo.core.utility.JSONService;
 import io.serateam.stewboo.core.utility.Sample;
 import io.serateam.stewboo.core.utility.SharedVariables;
 
 // set up processes go here (config setup, authentication, file integrity checks, etc)
+import java.util.ArrayList;
+import java.util.List;
 public class StewBoo
 {
     public static void initializeCore()
     {
         Sample s = new Sample();
-        JSONService.serializeAndWriteToFile(SharedVariables.test, s);
-        Sample newS = JSONService.deserialize(SharedVariables.test, Sample.class);
+        JSONService.serializeAndWriteToFile(SharedVariables.Path.test, s);
+        Sample newS = JSONService.deserialize(SharedVariables.Path.test, Sample.class);
         System.out.println(newS);
+
+        // For simplicity, we will be opting to grouping our services into a list
+        List<IService> services = new ArrayList<>();
+        // TODO: Add more services if need be.
+        services.add(PomodoroService.getInstance());
+
+        for(IService service : services)
+        {
+            System.out.println("Initializing service: " + service.getClass().getSimpleName());
+            service.initializeService();
+        }
+
     }
 }
