2f1e51cc0e44fbc9e4e908b6b125289aa56d08c0
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-20 05:37:46 +0800

    Progress Bar and choices for choosing time minutes for pomodoro, quick break, and long break sessions

--- StewBoo/src/main/java/io/serateam/stewboo/ui/menus/pomodoro/PomodoroMenuController.java
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/menus/pomodoro/PomodoroMenuController.java
@@ -7,10 +7,17 @@ import io.serateam.stewboo.core.services.pomodoro.PomodoroSessionState;
 import io.serateam.stewboo.ui.SharedVariables;
 import io.serateam.stewboo.ui.menus.IMenu;
 import io.serateam.stewboo.ui.utility.MusicPlayer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -24,6 +31,11 @@ public class PomodoroMenuController implements Initializable, IMenu, IPomodoroLi
     @FXML private Text txt_pomodoroCollected;
     @FXML private JFXButton btn_startTimer;
     @FXML private JFXButton btn_stopTimer;
+    @FXML private TextField textField_pomodoroMinutes;
+    @FXML private TextField textField_shortBreakMinutes;
+    @FXML private TextField textField_longBreakMinutes;
+    @FXML private Text txt_errorIncorrectInput;
+    @FXML private ProgressBar progressBar_timeUntilFinish;
 
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
     private static PomodoroService service;
@@ -33,9 +45,14 @@ public class PomodoroMenuController implements Initializable, IMenu, IPomodoroLi
     {
         service = PomodoroService.getInstance();
         service.addListener(this);
+        btn_stopTimer.setDisable(true);
 
         btn_startTimer.setOnMouseClicked(e -> onStartPomodoroClick());
-        btn_stopTimer.setOnMouseClicked(e -> onStopPomodoroClick());
+        btn_stopTimer.setOnMouseClicked(e -> {
+            btn_startTimer.setDisable(false);
+            btn_stopTimer.setDisable(true);
+            onStopPomodoroClick();
+        });
     }
     private void onStartPomodoroClick()
     {
@@ -49,11 +66,19 @@ public class PomodoroMenuController implements Initializable, IMenu, IPomodoroLi
     // region IPomodoroListener Implementation Methods
 
     @Override
-    public void onTimerUpdate(long remainingSeconds)
+    public void onTimerUpdate(long remainingSeconds, long initialSeconds)
     {
         System.out.println("Time left: " + remainingSeconds + " seconds");
         txt_timer.setText(formatSecondsToTime(remainingSeconds));
-        // progress bar.  reminaing seconds to zero
+
+//        progressBar_timeUntilFinish.setProgress((1 - ( (double) remainingSeconds / initialSeconds)));
+//        remaining seconds to zero
+        Timeline timeline = new Timeline();
+        KeyValue keyValue = new KeyValue(progressBar_timeUntilFinish.progressProperty(),
+                (1 - ( (double) remainingSeconds / initialSeconds)));
+        KeyFrame keyFrame = new KeyFrame(new Duration(500), keyValue);
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
 
     }
 
@@ -83,12 +108,16 @@ public class PomodoroMenuController implements Initializable, IMenu, IPomodoroLi
     {
         System.out.println("Timer state changed to: " + newState);
         txt_currentSession.setText("Current Session: " + identifySessionState(newState));
+        progressBar_timeUntilFinish.setProgress(0);
     }
 
     @Override
     public void onNewPomodoroTimeConfig(int workMinutes, int quickBreakMinutes, int longBreakMinutes)
     {
         System.out.printf("Work: %d, Quick Break: %d, Long Break %d%n", workMinutes, quickBreakMinutes, longBreakMinutes);
+        textField_pomodoroMinutes.setText(String.valueOf(workMinutes/60));
+        textField_shortBreakMinutes.setText(String.valueOf(quickBreakMinutes/60));
+        textField_longBreakMinutes.setText(String.valueOf(longBreakMinutes/60));
     }
 
     // endregion
@@ -97,8 +126,44 @@ public class PomodoroMenuController implements Initializable, IMenu, IPomodoroLi
 
     private void startPomodoro()
     {
-        System.out.println("Session started...");
-        service.startPomodoroSession();
+        try
+        {
+            validateNumericalTextField(textField_pomodoroMinutes);
+            validateNumericalTextField(textField_shortBreakMinutes);
+            validateNumericalTextField(textField_longBreakMinutes);
+
+            int pomMinutes;
+            int shortMinutes;
+            int longMinutes;
+
+            if(textField_pomodoroMinutes.getText().isEmpty())
+                pomMinutes = PomodoroService.DEFAULT_POMODORO_MINUTES;
+            else
+                pomMinutes = Integer.parseInt(textField_pomodoroMinutes.getText())*60;
+
+            if(textField_shortBreakMinutes.getText().isEmpty())
+                shortMinutes = PomodoroService.DEFAULT_POMODORO_BREAK_TIME;
+            else
+                shortMinutes = Integer.parseInt(textField_shortBreakMinutes.getText())*60;
+
+            if(textField_longBreakMinutes.getText().isEmpty())
+                longMinutes = PomodoroService.DEFAULT_POMODORO_LONG_BREAK_TIME;
+            else
+                longMinutes = Integer.parseInt(textField_longBreakMinutes.getText())*60;
+
+            setNewUserPomodoroTime(pomMinutes, shortMinutes, longMinutes);
+
+            System.out.println("Session started...");
+
+            btn_startTimer.setDisable(true);
+            btn_stopTimer.setDisable(false);
+            service.startPomodoroSession();
+        }
+        catch(IOException e)
+        {
+            System.err.println("POMODORO: Invalid text fields");
+        }
+
     }
     private void stopTimer()
     {
@@ -109,6 +174,27 @@ public class PomodoroMenuController implements Initializable, IMenu, IPomodoroLi
     // endregion
 
     // region Utility Methods
+
+    private void setNewUserPomodoroTime(int pomodoroMinutes, int shortMinutes, int longMinutes)
+    {
+        service.saveNewConfigTime(pomodoroMinutes, shortMinutes, longMinutes);
+    }
+
+    private void validateNumericalTextField(TextField tf) throws IOException
+    {
+        String invalidInput_CSS = "-fx-border-color: red; -fx-border-width: 2;";
+
+        if(!tf.getText().isEmpty()
+        && (!tf.getText().matches("^[1-9]\\d*$") || Integer.parseInt(tf.getText()) > 1440))
+        {
+            txt_errorIncorrectInput.setOpacity(1);
+            tf.setStyle(invalidInput_CSS);
+            throw new IOException();
+        }
+        // reset the styles in case the elements had the error styling
+        txt_errorIncorrectInput.setOpacity(0);
+        tf.setStyle("");
+    }
 
     /**
      * @return Time-formatted string using the format {@code HH:MM:ss}.
@@ -130,9 +216,9 @@ public class PomodoroMenuController implements Initializable, IMenu, IPomodoroLi
     {
         return switch (state)
         {
-            case WORK_SESSION -> "Work Session (25 minutes)";
-            case QUICK_BREAK -> "Quick Break (5 minutes)";
-            case LONG_BREAK -> "Long Break (15 minutes)";
+            case WORK_SESSION -> "Pomodoro Session";
+            case QUICK_BREAK -> "Quick Break";
+            case LONG_BREAK -> "Long Break";
         };
     }
 
