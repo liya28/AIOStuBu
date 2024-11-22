5c268af17c85ec42fa9717be81b2455f2504047e
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-22 21:26:23 +0800

    initial commit

--- StewBoo/src/main/java/io/serateam/stewboo/ui/menus/calendar/CalendarMenuController.java
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/menus/calendar/CalendarMenuController.java
@@ -1,13 +1,16 @@
 package io.serateam.stewboo.ui.menus.calendar;
 
+import io.serateam.stewboo.ui.menus.IMenu;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CalendarController implements Initializable {
+public class CalendarMenuController implements Initializable, IMenu
+{
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
         throw new RuntimeException("Uninitialized method exception");
     }
 }
