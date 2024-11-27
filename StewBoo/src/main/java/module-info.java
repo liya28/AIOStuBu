7b8c6947222471decfe91ac96836ce4776add0e3
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-28 00:14:41 +0800

    reordered modules

--- StewBoo/src/main/java/module-info.java
+++ StewBoo/src/main/java/module-info.java
@@ -1,21 +1,21 @@
 module io.serateam.stewboo {
-    requires com.jfoenix;
+    requires java.desktop;
     requires javafx.fxml;
     requires javafx.media;
-    requires org.controlsfx.controls;
-    requires atlantafx.base;
+    requires com.jfoenix;
     requires com.google.gson;
-    requires java.desktop;
+    requires atlantafx.base;
+    requires org.controlsfx.controls;
 
+    opens io.serateam.stewboo.core.utility to com.google.gson;
     opens io.serateam.stewboo.core.services.pomodoro to com.google.gson;
     opens io.serateam.stewboo.core.services.todolist to com.google.gson;
     opens io.serateam.stewboo.ui to javafx.fxml;
     opens io.serateam.stewboo.ui.menus to javafx.fxml;
+    opens io.serateam.stewboo.ui.menus.todolist to javafx.fxml;
     opens io.serateam.stewboo.ui.menus.pomodoro to javafx.fxml;
-    opens io.serateam.stewboo.core.utility to com.google.gson;
     exports io.serateam.stewboo.ui;
     exports io.serateam.stewboo.ui.menus;
     exports io.serateam.stewboo.ui.menus.pomodoro;
     exports io.serateam.stewboo.ui.menus.todolist;
-    opens io.serateam.stewboo.ui.menus.todolist to javafx.fxml;
 }
