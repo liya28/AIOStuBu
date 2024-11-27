868541ce177e918c298cdf8d05af06f31f3d7f9b
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-28 00:51:05 +0800

    refactor file location of stubulist.json

--- StewBoo/src/main/java/io/serateam/stewboo/core/utility/SharedVariables.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/utility/SharedVariables.java
@@ -4,11 +4,11 @@ public class SharedVariables
 {
     // TODO: Add paths here
     public static final String test = "test.json";
-    public static final String path_todoList = "stubulist.json";
     public static class Path
     {
         public static final String mainUserDirectory = "aiostubu/";
         public static final String test = mainUserDirectory + "test.json";
         public static final String pomodoroUserConfig = mainUserDirectory + "pomodoro_config.json";
+        public static final String path_todoList = mainUserDirectory + "stubulist.json";
     }
 }
