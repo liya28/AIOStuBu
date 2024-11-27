3984c6a80f33c1064492d8512e8c7cfa447e83dc
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-11-27 20:38:28 +0800

    import NotesService

--- StewBoo/src/main/java/io/serateam/stewboo/core/StewBoo.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/StewBoo.java
@@ -1,6 +1,7 @@
 package io.serateam.stewboo.core;
 
 import io.serateam.stewboo.core.services.IService;
+import io.serateam.stewboo.core.services.notes.NotesService;
 import io.serateam.stewboo.core.services.pomodoro.PomodoroService;
 import io.serateam.stewboo.core.utility.JSONService;
 import io.serateam.stewboo.core.utility.Sample;
