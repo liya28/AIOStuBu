ecc9441a2f453ae4d37975c29af64de02a8a7cc0
muhadma <harleyreyes456@gmail.com>
2024-11-27 23:01:09 +0800

    remove the constructor and made changes to follow the code bible

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/flashcard/Card.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/flashcard/Card.java
@@ -2,20 +2,24 @@ package io.serateam.stewboo.core.services.flashcard;
 
 import io.serateam.stewboo.core.utility.ISerializable;
 
-public class Card implements ISerializable {
+public class Card implements ISerializable
+{
     private String question;
     private String answer;
 
-    public Card(String question, String answer) {
+    public Card(String question, String answer)
+    {
         this.question = question;
         this.answer = answer;
     }
 
-    public String getQuestion() {
+    public String getQuestion()
+    {
         return question;
     }
 
-    public String getAnswer() {
+    public String getAnswer()
+    {
         return answer;
     }
 }
