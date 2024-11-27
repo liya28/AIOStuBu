5212c7ffe01f90a660a2c530d222f133715fd344
muhadma <harleyreyes456@gmail.com>
2024-11-27 23:03:20 +0800

    made changes to follow the code bible

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/flashcard/Deck.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/flashcard/Deck.java
@@ -5,32 +5,39 @@ import io.serateam.stewboo.core.utility.ISerializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Deck implements ISerializable {
+public class Deck implements ISerializable
+{
     private String name;
     private List<Card> flashCards;
 
-    public Deck(String name) {
+    public Deck(String name)
+    {
         this.name = name;
         this.flashCards = new ArrayList<>();
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public List<Card> getFlashCards() {
+    public List<Card> getFlashCards()
+    {
         return flashCards;
     }
 
-    public int getflashCardsCount() {
+    public int getflashCardsCount()
+    {
         return flashCards.size();
     }
 
-    public void addflashCard(Card card) {
+    public void addflashCard(Card card)
+    {
         flashCards.add(card);
     }
 
-    public void removeflashCard(Card card) {
+    public void removeflashCard(Card card)
+    {
         flashCards.remove(card);
     }
 }
