696c6e60eaf76832b298c8f3c4e8b6620d799b02
muhadma <harleyreyes456@gmail.com>
2024-11-29 00:19:03 +0800

    added an edit button so that when pressed it will pop a new window for it to be edited

--- StewBoo/src/main/java/io/serateam/stewboo/ui/menus/flashcards/CardViewController.java
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/menus/flashcards/CardViewController.java
@@ -1,17 +1,26 @@
 package io.serateam.stewboo.ui.menus.flashcards;
 
 import io.serateam.stewboo.core.services.flashcard.Card;
+import io.serateam.stewboo.core.services.flashcard.FlashCardService;
+import io.serateam.stewboo.ui.SharedVariables;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.util.Collections;
 import java.util.List;
 
 public class CardViewController
 {
+    FlashCardService service = FlashCardService.getInstance();
+
     @FXML private Label QuestionLabel;
     @FXML private Label AnswerLabel;
     @FXML private Label counterLabel;
@@ -20,6 +29,7 @@ public class CardViewController
     @FXML private Button previousButton;
     @FXML private Button shuffleButton;
     @FXML private Button showAnswerButton;
+    @FXML private Button btn_edit;
 
     private List<Card> flashCards;
     private int currentIndex;
@@ -38,6 +48,29 @@ public class CardViewController
         else
         {
             loadFlashcard(currentIndex);
+        }
+    }
+
+    @FXML
+    private void editFlashcard()
+    {
+        try{
+            FXMLLoader loader = new FXMLLoader(SharedVariables.url_path_flashcardsEditFxml);
+            Parent root = loader.load();
+
+            CardCreationController con = loader.getController();
+            con.setFlashcard(flashCards.get(currentIndex));
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Edit Flashcard");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+
+            service.saveDecksToFile();
+            loadFlashcard(currentIndex);
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
