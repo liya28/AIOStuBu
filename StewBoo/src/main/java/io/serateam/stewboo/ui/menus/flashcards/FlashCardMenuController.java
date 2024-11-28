7d73a6cbaaf49292f77a03e24c8edc36905fd1dd
muhadma <harleyreyes456@gmail.com>
2024-11-28 22:38:20 +0800

    deleted local ShowAlert

--- StewBoo/src/main/java/io/serateam/stewboo/ui/menus/flashcards/FlashCardMenuController.java
+++ StewBoo/src/main/java/io/serateam/stewboo/ui/menus/flashcards/FlashCardMenuController.java
@@ -152,15 +152,6 @@ public class FlashCardMenuController implements IMenu
         deckListView.refresh();
     }
 
-    private void showAlert(String title, String message)
-    {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
-    }
-
     @FXML
     private void deleteDeck()
     {
