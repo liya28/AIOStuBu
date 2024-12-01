a21f439bb28ff7d24c321a725457efb48a84f35f
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-12-02 03:26:08 +0800

    reflections for calendar feature

--- StewBoo/src/main/java/module-info.java
+++ StewBoo/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module io.serateam.stewboo {
     requires com.calendarfx.view;
 
     opens io.serateam.stewboo.core.services.pomodoro to com.google.gson;
+    opens io.serateam.stewboo.core.services.calendar to com.google.gson;
     opens io.serateam.stewboo.ui to javafx.fxml;
     opens io.serateam.stewboo.ui.menus to javafx.fxml;
     opens io.serateam.stewboo.ui.menus.pomodoro to javafx.fxml;
