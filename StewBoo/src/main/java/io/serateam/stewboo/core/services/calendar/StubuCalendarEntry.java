b5d72a366ebc9f7d4672426e691d15336d331c11
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-12-02 03:28:04 +0800

    create StubuCalendarEntry class

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/calendar/StubuCalendarEntry.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/calendar/StubuCalendarEntry.java
@@ -1,8 +1,65 @@
 package io.serateam.stewboo.core.services.calendar;
 
-import com.calendarfx.model.Entry;
+import java.time.LocalDateTime;
 
-public class StubuCalendarEntry extends Entry<StubuCalendarEvent>
+public class StubuCalendarEntry
 {
+    // FIELDS
+    private String id;
+    private String title;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String location;
+    private boolean fullDay;
+    private String recurrenceRule;
 
+    // CONSTRUCTOR
+    public StubuCalendarEntry(String id, String title,
+                              LocalDateTime startAsLocalDateTime, LocalDateTime endAsLocalDateTime,
+                              String location, boolean fullDay, String recurrenceRule)
+    {
+        this.id = id;
+        this.title = title;
+        this.startDate = startAsLocalDateTime;
+        this.endDate = endAsLocalDateTime;
+        this.location = location;
+        this.fullDay = fullDay;
+        this.recurrenceRule = recurrenceRule;
+    }
+
+    // METHODS
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public LocalDateTime getStartDate()
+    {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate()
+    {
+        return endDate;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public boolean isFullDay()
+    {
+        return fullDay;
+    }
+
+    public String getRecurrenceRule()
+    {
+        return recurrenceRule;
+    }
 }
