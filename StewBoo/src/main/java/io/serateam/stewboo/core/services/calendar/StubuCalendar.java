cbeb1bb51c619fca16cf6a58152c470e5b2cf5b4
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-12-02 03:28:40 +0800

    create StubuCalendar class

--- StewBoo/src/main/java/io/serateam/stewboo/core/services/calendar/StubuCalendar.java
+++ StewBoo/src/main/java/io/serateam/stewboo/core/services/calendar/StubuCalendar.java
@@ -1,8 +1,41 @@
 package io.serateam.stewboo.core.services.calendar;
 
-import com.calendarfx.model.Calendar;
+import io.serateam.stewboo.core.utility.ISerializable;
 
-public class StubuCalendar extends Calendar
+import java.util.ArrayList;
+import java.util.List;
+
+public class StubuCalendar implements ISerializable
 {
+    // FIELDS
+    private String name;
+    private List<StubuCalendarEntry> calendarEntryList;
 
+    // CONSTRUCTOR
+    public StubuCalendar(String name)
+    {
+        this.name = name;
+        calendarEntryList = new ArrayList<>();
+    }
+
+    // METHODS
+    public String getName()
+    {
+        return name;
+    }
+
+    public List<StubuCalendarEntry> getEntryList()
+    {
+        return calendarEntryList;
+    }
+
+    public void addEntry(StubuCalendarEntry calendarEntry)
+    {
+        calendarEntryList.add(calendarEntry);
+    }
+
+    public void removeEntry(StubuCalendarEntry calendarEntry)
+    {
+        calendarEntryList.remove(calendarEntry);
+    }
 }
