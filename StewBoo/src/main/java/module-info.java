1a331dbe924b9077f087129690404af8a7a3c41e
Lindrew <66846143+GReturn@users.noreply.github.com>
2024-12-04 22:30:20 +0800

    remove duplicates

--- StewBoo/src/main/java/module-info.java
+++ StewBoo/src/main/java/module-info.java
@@ -19,8 +19,6 @@ module io.serateam.stewboo {
     opens io.serateam.stewboo.ui.menus.todolist to javafx.fxml;
     opens io.serateam.stewboo.ui.menus.pomodoro to javafx.fxml;
     opens io.serateam.stewboo.ui.menus.flashcards to javafx.fxml;
-    opens io.serateam.stewboo.ui.menus.notes to javafx.fxml;
-    opens io.serateam.stewboo.core.utility to com.google.gson;
     exports io.serateam.stewboo.ui;
     exports io.serateam.stewboo.ui.menus;
     exports io.serateam.stewboo.ui.menus.pomodoro;
